48e9ad2075dcfa48337298902f2c8b0252897432
Krishna Mandal <mandalkrishna739@gmail.com>
2024-02-12 14:52:43 +0545

    Break and continue using for loop

--- windows/practice/alliinoneprac/loops.c
+++ windows/practice/alliinoneprac/loops.c
@@ -4,8 +4,10 @@ int main()
     // For loop
     int i = 1, n;
 
+    /*
     printf("Enter an integer: ");
     scanf("%d", &n);
+    */
 
     /*
     for (i = 1; i <= 10; i++)
@@ -24,11 +26,25 @@ int main()
     */
 
     // Do_While loop
+    /*
     do
     {
         printf("%d x %d = %d\n", n, i, n * i);
         i++;
     } while (i <= 10);
+    */
+
+    //    break & continue
+    for (i = 0; i <= 10; i++)
+    {
+        printf("%d\n", i);
+        if (i == 4)
+        {
+            printf("Breaking  at %d\n", i);
+            // break;
+            continue;
+        }
+    }
 
     return 0;
 }
