96e9d8137398c2aa772bd3c61500903f50e43c37
Krishna Mandal <56486342+Krish123-lang@users.noreply.github.com>
2024-02-24 08:45:24 +0545

    Update cmtoother.c

--- Examples/CIntroEx/cmtoother.c
+++ Examples/CIntroEx/cmtoother.c
@@ -13,3 +13,9 @@ int main()
     printf("%lf cm in kilometres is: %lf km \n", centimeters, kilometers);
     return 0;
 }
+
+/*
+Enter length in CM: 500
+500.000000 cm in metres is: 5.000000 meters 
+500.000000 cm in kilometres is: 0.005000 km 
+*/
